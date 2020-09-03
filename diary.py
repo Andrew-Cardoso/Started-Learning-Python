2d48bbf6c47cdd72332fb731103f3d6f08d7a8f1
Andrew Cardoso <andrewgcardoso@gmail.com>
2020-09-03 20:57:24 -0300

    test

--- diary.py
+++ diary.py
@@ -2,11 +2,12 @@ question = ("how", "why", "what", "could", "should", "can")
 text = []
 while True:
      phrase = input("Say something: ").lower()
-     if phrase == "\\end":
-          print(" ".join(text))
+     if phrase == "\end":
           break
      if phrase.startswith(question):
           phrase += "?"
      else:
           phrase += "."
      text.append(phrase.capitalize())
+
+print(" ".join(text))
